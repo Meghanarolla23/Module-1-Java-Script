8b903a05c927a07abdbc72b3ca8f256ced261eb8
Meghanarolla23 <meghanarolla23@gmail.com>
2025-05-27 01:19:33 +0530

    Que2

---
+++ Que2.js
@@ -0,0 +1,7 @@
+const eventName = "Music Fest";
+const eventDate = "2025-06-15";
+let availableSeats = 50;
+
+console.log(`${eventName} is on ${eventDate}. Seats available: ${availableSeats}`);
+
+availableSeats--; // One user registered

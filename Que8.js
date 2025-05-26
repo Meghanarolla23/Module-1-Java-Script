4c10d3ff2e076e77532a90b469c542d121d1369c
Meghanarolla23 <meghanarolla23@gmail.com>
2025-05-27 01:22:08 +0530

    Que8

---
+++ Que8.js
@@ -0,0 +1,13 @@
+function register(eventName) {
+    console.log(`Registered for ${eventName}`);
+  }
+  
+  function filterByCategory(category) {
+    console.log(`Filtering by ${category}`);
+  }
+  
+  function searchEvent(e) {
+    if (e.key === "Enter") {
+      console.log(`Searching: ${e.target.value}`);
+    }
+  }
